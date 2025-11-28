3bf1f722e1eb588e81b733d3ff8a3ee446ab3c9e
Hesamedine Jannesari <hesamverse@proton.me>
2025-11-28 04:18:19 -0500

    Central Header Library – include/web3c/web3c.h

---
+++ include/web3c/web3c.h
@@ -0,0 +1,14 @@
+#ifndef WEB3C_WEB3C_H
+#define WEB3C_WEB3C_H
+
+/*
+ * Web3C - Minimal C toolkit for Web3 / Ethereum.
+ *
+ * This header aggregates the public API of the library.
+ * Users should include only this file in most cases.
+ */
+
+#include "abi.h"
+#include "hex.h"
+
+#endif /* WEB3C_WEB3C_H */
